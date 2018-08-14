c16fecb7f3def169356897de0fb0d1cbf784f7cc
thanh4890 <thanh4890@gmail.com>
2018-08-14 17:03:03 +0700

    add some commands for webpack mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js", 
   output: {
     path: path.resolve(__dirname, "build-webpack"),
