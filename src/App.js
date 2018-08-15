9d356222a102af6bb6a029ce115f024eef980d87
thanh4890 <thanh4890@gmail.com>
2018-08-15 17:57:13 +0700

    Use list from ndt-react-list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
 import style from './App.css'
+import List from 'ndt-react-list'
+
+const posts = [
+  {
+    id: 1,
+    title: 'Create Apps with No Configuration',
+  },
+  {
+    id: 2,
+    title: 'Mixins Considered Harmful',
+  },
+]
 
 const App = () => (
-  <div className="app">App</div>
+  <div className="app">
+    <List collection={posts} titleKey="title" />
+  </div>
 )
 
 export default App
--- stories/list.js
+++ stories/list.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import List from '../src/components/List'
+import List from 'ndt-react-list'
 
 const posts = [
   {
