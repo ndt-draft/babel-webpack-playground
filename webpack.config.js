a8506e3c292ea94cc3007c5c0f8fba9075282eba
thanh4890 <thanh4890@gmail.com>
2018-08-23 14:20:19 +0700

    get mode flag in webpack.config.js, minify css for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,63 @@
 const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.js", 
-  output: {
-    path: path.resolve(__dirname, "build-webpack"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
-      }
+module.exports = (env, argv) => {
+  // @see https://github.com/webpack/webpack/issues/6460#issuecomment-364286147
+  const PROD_MODE = argv.mode === 'production'
+
+  return {
+    entry: "./src/index.js", 
+    output: {
+      path: path.resolve(__dirname, "build-webpack"),
+      filename: "bundle.js",
+    },
+    optimization: {
+      // @see https://github.com/webpack-contrib/mini-css-extract-plugin#minimizing-for-production
+      minimizer: [
+        new UglifyJsPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: true // set to true if you want JS source maps
+        }),
+        new OptimizeCSSAssetsPlugin({})
+      ]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: "babel-loader"
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader"
+            }
+          ]
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader"
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./public/index.html",
+        filename: "./index.html"
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      })
     ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html"
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
-  ]
+  }
 }
